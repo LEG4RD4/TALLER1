c7885bef62efa3531eae1d6f98fa1bbb4820bf14
LEG4RD4 <legarda8545@gmail.com>
2026-03-03 21:50:23 -0500

    9

--- cafe-infromativo/resources/views/contacto.blade.php
+++ cafe-infromativo/resources/views/contacto.blade.php
@@ -6,7 +6,7 @@
 
 <p> Dirección: Calle 15 # 8-42, Pasto, Nariño</p>
 <p> Teléfono: +57 300 000 0000</p>
-<p> Correo: cafecolombia@ejemplo.com</p>
+<p> Correo: cafecolombia@gmail.com</p>
 <p> Horario: Lunes a Viernes 7am - 8pm | Sábados 8am - 6pm</p>
 
 @endsection
