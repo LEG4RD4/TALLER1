72f23f728d4d3a60ea32fcfd36595448169e56f5
LEG4RD4 <legarda8545@gmail.com>
2026-03-03 21:45:03 -0500

    7 video

--- cafe-infromativo/resources/views/inicio.blade.php
+++ cafe-infromativo/resources/views/inicio.blade.php
@@ -8,6 +8,6 @@
 <img src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?w=500" width="500" alt="Café">
 
 <h4>🎬 Video: El café colombiano</h4>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/jxKKSCa5IQ4" title="Café de Colombia" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/watch?v=YqhEVDxZf1o" title="Café de Colombia" allowfullscreen></iframe>
 
 @endsection
