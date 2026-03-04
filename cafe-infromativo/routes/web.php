9c7f3d2a4f12ad615a5f16477a5d8137572d0110
LEG4RD4 <legarda8545@gmail.com>
2026-03-03 21:17:46 -0500

    2 guardado

---
+++ cafe-infromativo/routes/web.php
@@ -0,0 +1,21 @@
+routes/web.php
+Reemplazar el contenido por:
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('inicio');
+})->name('inicio');
+
+Route::get('/menu', function () {
+    return view('menu');
+})->name('menu');
+
+Route::get('/nosotros', function () {
+    return view('nosotros');
+})->name('nosotros');
+
+Route::get('/contacto', function () {
+    return view('contacto');
+})->name('contacto');
