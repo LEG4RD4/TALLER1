c558b5eedd0394daa13ea34347a329058bfbd04e
LEG4RD4 <legarda8545@gmail.com>
2026-03-17 21:39:56 -0500

    7

--- cafe-infromativo/app/Http/Controllers/PaginaController.php
+++ cafe-infromativo/app/Http/Controllers/PaginaController.php
@@ -6,8 +6,23 @@ use Illuminate\Http\Request;
 
 class PaginaController extends Controller
 {
+    public function inicio()
+    {
+        return view('inicio');
+    }
+
+    public function menu()
+    {
+        return view('menu');
+    }
+
     public function nosotros()
     {
-    return view('nosotros');
+        return view('nosotros');
+    }
+
+    public function contacto()
+    {
+        return view('contacto');
     }
 }
